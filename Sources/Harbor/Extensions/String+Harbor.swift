973035e3bdd8feb525de8c420379b395f71e4c68
Stanislas Chevallier <contact@stanislaschevallier.fr>
2023-02-18 17:57:15 +0100

    Fix Docker config generation

--- Sources/Harbor/Extensions/String+Harbor.swift
+++ Sources/Harbor/Extensions/String+Harbor.swift
@@ -34,3 +34,10 @@ extension String {
         return self
     }
 }
+
+extension String {
+    static func randomAlphaNumeric(length: Int) -> String {
+      let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+      return String((0..<length).map{ _ in letters.randomElement()! })
+    }
+}

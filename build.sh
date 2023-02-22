5f8ab0599bb2ff450f779f6cca677fe58ed7bca2
Stanislas Chevallier <contact@stanislaschevallier.fr>
2023-02-22 17:06:41 +0100

    Improve build to archive binary and resources

--- build.sh
+++ build.sh
@@ -4,13 +4,14 @@ set -e
 
 if [ "x$1" = "xrelease" ]; then
     # cleanup
-    rm -rf .build
+    rm -rf .build/ build/
 
     # build for macOS
     swift build --arch arm64 --arch x86_64 -c release
     mkdir -p "build/macOS"
     rsync -ar ".build/apple/Products/Release/harbor"               "build/macOS"
     rsync -ar ".build/apple/Products/Release/Harbor_Harbor.bundle" "build/macOS"
+    tar -C build/macOS -czvf build/macOS.tar.gz .
 
     # build for linux
     if ! command -v docker &> /dev/null; then
@@ -23,12 +24,14 @@ if [ "x$1" = "xrelease" ]; then
     mkdir -p "build/linux-amd64"
     rsync -ar ".build/x86_64-unknown-linux-gnu/release/harbor"                   "build/linux-amd64"
     rsync -ar ".build/x86_64-unknown-linux-gnu/release/Harbor_Harbor.resources"  "build/linux-amd64"
+    tar -C build/linux-amd64 -czvf build/linux-amd64.tar.gz .
 
     docker container rm -f harbor-linux > /dev/null
     docker run -it --name harbor-linux --platform linux/arm64/v8 -v $(pwd):/harbor swift:latest /bin/bash -c "cd harbor && swift build -c release"
     mkdir -p "build/linux-arm64"
     rsync -ar ".build/aarch64-unknown-linux-gnu/release/harbor"                  "build/linux-arm64"
     rsync -ar ".build/aarch64-unknown-linux-gnu/release/Harbor_Harbor.resources" "build/linux-arm64"
+    tar -C build/linux-arm64 -czvf build/linux-arm64.tar.gz .
 
 else
     swift build --arch arm64 --arch x86_64 -c debug

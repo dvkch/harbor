376733bb3684671cee1d867b9558015f203ff11b
Stanislas Chevallier <contact@stanislaschevallier.fr>
2023-02-23 00:52:59 +0100

    Improve build script output

--- build.sh
+++ build.sh
@@ -7,11 +7,13 @@ if [ "x$1" = "xrelease" ]; then
     rm -rf .build/ build/
 
     # build for macOS
-    swift build --arch arm64 --arch x86_64 -c release
+    echo ""
+    echo "Building for macOS..."
+    swift build --arch arm64 --arch x86_64 -c release > /dev/null
     mkdir -p "build/macOS"
     rsync -ar ".build/apple/Products/Release/harbor"               "build/macOS"
     rsync -ar ".build/apple/Products/Release/Harbor_Harbor.bundle" "build/macOS"
-    tar -C build/macOS -czvf build/macOS.tar.gz .
+    tar -C build/macOS -czf build/macOS.tar.gz .
 
     # build for linux
     if ! command -v docker &> /dev/null; then
@@ -19,19 +21,23 @@ if [ "x$1" = "xrelease" ]; then
         exit -1
     fi
 
+    echo ""
+    echo "Building for Linux x64..."
     docker container rm -f harbor-linux > /dev/null
-    docker run -it --name harbor-linux --platform linux/amd64    -v $(pwd):/harbor swift:latest /bin/bash -c "cd harbor && swift build -c release"
+    docker run -it --name harbor-linux --platform linux/amd64    -v $(pwd):/harbor swift:latest /bin/bash -c "cd harbor && swift build -c release > /dev/null"
     mkdir -p "build/linux-amd64"
     rsync -ar ".build/x86_64-unknown-linux-gnu/release/harbor"                   "build/linux-amd64"
     rsync -ar ".build/x86_64-unknown-linux-gnu/release/Harbor_Harbor.resources"  "build/linux-amd64"
-    tar -C build/linux-amd64 -czvf build/linux-amd64.tar.gz .
+    tar -C build/linux-amd64 -czf build/linux-amd64.tar.gz .
 
+    echo ""
+    echo "Building for Linux ARM64..."
     docker container rm -f harbor-linux > /dev/null
-    docker run -it --name harbor-linux --platform linux/arm64/v8 -v $(pwd):/harbor swift:latest /bin/bash -c "cd harbor && swift build -c release"
+    docker run -it --name harbor-linux --platform linux/arm64/v8 -v $(pwd):/harbor swift:latest /bin/bash -c "cd harbor && swift build -c release > /dev/null"
     mkdir -p "build/linux-arm64"
     rsync -ar ".build/aarch64-unknown-linux-gnu/release/harbor"                  "build/linux-arm64"
     rsync -ar ".build/aarch64-unknown-linux-gnu/release/Harbor_Harbor.resources" "build/linux-arm64"
-    tar -C build/linux-arm64 -czvf build/linux-arm64.tar.gz .
+    tar -C build/linux-arm64 -czf build/linux-arm64.tar.gz .
 
 else
     swift build --arch arm64 --arch x86_64 -c debug
